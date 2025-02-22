6912fba645430cfea8fa2f2b95cf88277ddd1e92
smoenter <staceymoenter@gmail.com>
2025-02-22 13:38:17 -0500

    added the interface candidate info

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,3 +1,13 @@
 
 
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    name: string;
+    username: string;
+    location: string;
+    avatar: string;
+    email: string;
+    html_url: string;
+    company: string;
+   }
+     
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import Nav from '../components/Nav';
 
 const CandidateSearch = () => {
   return <h1>Candidate Search</h1>;
+  const [currentCandidate, setcurrentCandidate] = useState({});
 };
 
 export default CandidateSearch;
