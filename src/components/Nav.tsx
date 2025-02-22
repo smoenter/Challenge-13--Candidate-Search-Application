7ca14542df0244f2a54bb06480e3e5532c4b737a
smoenter <staceymoenter@gmail.com>
2025-02-21 19:43:42 -0500

    intial coding for the Nav.tsx with react-bootstrap

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,41 @@
+// add the link to react-bootstrap
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+
+
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+    <Card.Body>
+      <Card.Title>Candidate Search</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Location</ListGroup.Item>
+      <ListGroup.Item>Email</ListGroup.Item>
+      <ListGroup.Item>Bio</ListGroup.Item>
+    </ListGroup>
+    <Card.Body>
+    <Button variant="success">Success</Button>
+    <Button variant="danger">Danger</Button>
+    </Card.Body>
+  </Card>
   )
 };
 
 export default Nav;
+
+
+
+
+
+ 
+
+
